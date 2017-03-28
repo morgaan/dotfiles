7192801a3fa90ea18fd7cf7bff74b63542223ec3
Morgan CUGERONE <morgaan@gmail.com>
2017-03-29 00:21:43 +0200

    Fix paths in install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ rm -f .vim
 rm -f .vimrc
 rm -f .gitignore
 
-ln -s ~/dotfiles/vim .vim
-ln -s ~/dotfiles/vim/.vimrc .vimrc
+ln -s ~/dotfiles/.vim .vim
+ln -s ~/dotfiles/.vimrc .vimrc
 ln -s ~/dotfiles/.gitignore .gitignore
