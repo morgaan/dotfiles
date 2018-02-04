1b15768c0bdaa1134a3e323fc30bfaf37db74bdd
Morgan CUGERONE <morgaan@gmail.com>
2018-02-04 23:51:55 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 cd ~
-rm -f .vim
+rm -rf .vim
 rm -f .vimrc
+rm -rf .tmux
+rm -f .tmux.conf
+rm -f .ackrc
+rm -f .zshrc
 rm -f .gitignore
 
 ln -s ~/dotfiles/.vim .vim
 ln -s ~/dotfiles/.vimrc .vimrc
+ln -s ~/dotfiles/.tmux .tmux
+ln -s ~/dotfiles/.tmux.conf .tmux.conf
+ln -s ~/dotfiles/.ackrc .ackrc
 ln -s ~/dotfiles/.gitignore .gitignore
