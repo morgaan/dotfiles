5a859a98c17f1fcd14e6b5d14f4b1c56734c41e7
Morgan CUGERONE <morgaan@gmail.com>
2023-01-06 11:32:52 +0100

    Install Telescope plugin with basic remaps

--- .config/nvim/lua/morgan/packer.lua
+++ .config/nvim/lua/morgan/packer.lua
@@ -6,4 +6,11 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
 end)
