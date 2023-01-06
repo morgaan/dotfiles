2aacb30ddecaf882c54a7a445d2e2b102c2083ea
Morgan CUGERONE <morgaan@gmail.com>
2023-01-06 17:30:46 +0100

    Set settings structure

--- .config/nvim/lua/morgan/settings.lua
+++ .config/nvim/lua/morgan/settings.lua
@@ -1,3 +1,22 @@
+-- General configuration
+-- ---------------------
+
+
+-- Indentation
+-- -----------
+
+
+-- Search
+-- ------
+
+
+-- Swap and backup file strategy
+-- -----------------------------
+
+
+-- User Interface
+-- --------------
+
 vim.o.termguicolors = true
 
 vim.cmd('colorscheme solarized')
