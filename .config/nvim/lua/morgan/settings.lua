b1fc4f65551291be0712001fd93259d37e1d8a2d
Morgan CUGERONE <morgaan@gmail.com>
2023-01-06 17:42:44 +0100

    Enable project specific vim configs
    Enables the reading of .vimrc, .exrc and .gvimrc in the current
    directory.

--- .config/nvim/lua/morgan/settings.lua
+++ .config/nvim/lua/morgan/settings.lua
@@ -5,6 +5,7 @@ vim.api.nvim_exec('language en_US.utf-8', true)
 vim.o.encoding = 'utf-8'
 vim.o.backspace = 'indent,eol,start'
 vim.o.hidden = true
+vim.o.history = 1000
 
 -- Indentation
 -- -----------
