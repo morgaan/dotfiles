d3d9a047b0424cf758e5ecc79806f848e9fcc69d
Morgan CUGERONE <morgaan@gmail.com>
2023-01-10 20:10:31 +0100

    Add mappings to ease window navigation

--- .config/nvim/lua/morgan/remap.lua
+++ .config/nvim/lua/morgan/remap.lua
@@ -7,3 +7,9 @@ vim.keymap.set('n', 'k', 'gk')
 
 vim.keymap.set('n', '<leader>w', '<cmd>write!<CR>')
 vim.keymap.set('n', '<leader>q', '<cmd>quit<CR>')
+
+-- Easier window navigation.
+vim.keymap.set('', '<C-h>', '<C-w>h')
+vim.keymap.set('', '<C-j>', '<C-w>j')
+vim.keymap.set('', '<C-k>', '<C-w>k')
+vim.keymap.set('', '<C-l>', '<C-w>l')
