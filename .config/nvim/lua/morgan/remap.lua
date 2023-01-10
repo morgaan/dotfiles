d85824be0ea3d99c48ecf53e2344922d7630912d
Morgan CUGERONE <morgaan@gmail.com>
2023-01-10 20:02:30 +0100

    Add mappings to ease writing and quiting

--- .config/nvim/lua/morgan/remap.lua
+++ .config/nvim/lua/morgan/remap.lua
@@ -4,3 +4,6 @@ vim.keymap.set('n', '--', vim.cmd.Ex)
 
 vim.keymap.set('n', 'j', 'gj')
 vim.keymap.set('n', 'k', 'gk')
+
+vim.keymap.set('n', '<leader>w', '<cmd>write!<CR>')
+vim.keymap.set('n', '<leader>q', '<cmd>quit<CR>')
