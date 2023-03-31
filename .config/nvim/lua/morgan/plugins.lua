94fd2d6d39fb107b4bf0809514382e3357156fee
Morgan CUGERONE <morgaan@gmail.com>
2023-03-31 15:36:02 +0200

    Add vim text objects plugins

--- .config/nvim/lua/morgan/plugins.lua
+++ .config/nvim/lua/morgan/plugins.lua
@@ -4,4 +4,15 @@ vim.cmd [[packadd packer.nvim]]
 require('packer').startup(function(use)
 	-- Package manager (Packer can manage itself)
 	use 'wbthomason/packer.nvim'
+
+	-- Text objects
+	-- ------------
+
+	use('kana/vim-textobj-user')
+	use('kana/vim-textobj-entire')
+	use('kana/vim-textobj-line')
+	use('michaeljsmith/vim-indent-object')
+	use('whatyouhide/vim-textobj-xmlattr')
+	use('jceb/vim-textobj-uri')
+	use('wellle/targets.vim')
 end)
