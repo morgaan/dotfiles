22959948e71054229c9ba672cf7fb8e97c7bf982
Morgan CUGERONE <morgaan@gmail.com>
2023-03-31 11:41:19 +0200

    Add vim new config file + options & colorscheme config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,2 @@
+require('morgan.options')
+require('morgan.colorscheme')
---
+++ .config/nvim/lua/morgan/options.lua
@@ -0,0 +1,65 @@
+local opt = vim.opt
+local winopt = vim.wo
+
+-- General configuration
+-- ---------------------
+
+vim.api.nvim_exec('language en_US.utf-8', true)
+opt.encoding = 'utf-8'
+
+-- Indentation
+-- -----------
+
+opt.expandtab = false
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.shiftround = true
+
+-- Search
+-- ------
+
+opt.hlsearch = false
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Swap and backup file strategy
+-- -----------------------------
+
+opt.directory = os.getenv("HOME") .. "/.vim-directory//"
+opt.undodir = os.getenv("HOME") .. "/.vim-directory/undo//"
+opt.undofile = true
+opt.backup = false
+opt.writebackup = false
+
+-- User Interface
+-- --------------
+
+opt.termguicolors = true
+opt.title = true
+opt.number = true
+opt.splitright = true
+opt.splitbelow = true
+opt.listchars = 'tab:>~,space:·,trail:·,eol:¬'
+opt.cursorline = true
+opt.textwidth = 80
+opt.guifont = {'Victor Mono', 'h16'}
+opt.showmatch = true
+opt.omnifunc = 'syntaxcomplete#Complete'
+opt.guicursor = 'n-v-c-ci-i:block-Cursor-blinkwait1-blinkon150-blinkoff150'
+opt.background = 'dark'
+winopt.colorcolumn = '+1'
+winopt.signcolumn = 'yes'
+
+-- Don't offer to open certain files/folders
+opt.wildignore:append{"*.bmp", "*.gif", "*.ico", "*.jpg", "*.png", "*.ico"}
+opt.wildignore:append{"*.pdf", "*.psd"}
+opt.wildignore:append{"*.map", "*.min.css", "*.min.css"}
+opt.wildignore:append{"node_modules", "bower_components"}
+
+-- Outside vim file changes tracking
+opt.updatetime = 250
+
+-- Better completion experience
+opt.completeopt = 'menuone,noselect'
+
