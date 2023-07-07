29be362b7fd8ea4c10ca20d68ba7db699cae3d9b
Morgan CUGERONE <morgaan@gmail.com>
2023-07-07 21:02:00 +0200

    Enable jsdoc snippet for js files + Edit mappings for cmp

--- .config/nvim/after/plugin/completions.lua
+++ .config/nvim/after/plugin/completions.lua
@@ -7,23 +7,25 @@ local has_words_before = function()
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
+luasnip.filetype_extend("javascript", { "jsdoc" })
+
 require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
 	snippet = {
       -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        luasnip.lsp_expand(args.body) -- For `luasnip` users.
       end,
     },
-	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	mapping = {
+		['<c-space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c', }),
+		['<c-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+		['<c-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+		['<c-e>'] = cmp.mapping.abort(),
+		['<cr>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 
-		["<Tab>"] = cmp.mapping(function(fallback)
+		['<Tab>'] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
 				-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
@@ -35,9 +37,9 @@ cmp.setup({
 			else
 				fallback()
 			end
-		end, { "i", "s" }),
+		end, { 'i', 's' }),
 
-		["<S-Tab>"] = cmp.mapping(function(fallback)
+		['<S-Tab>'] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
 			elseif luasnip.jumpable(-1) then
@@ -45,8 +47,8 @@ cmp.setup({
 			else
 				fallback()
 			end
-		end, { "i", "s" })
-	}),
+		end, { 'i', 's' })
+	},
 	sources = cmp.config.sources({
       { name = 'nvim_lsp' },
       { name = 'luasnip' },
