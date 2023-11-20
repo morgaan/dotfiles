16267a0da6c7449b9a7ee3746888b97e73495ed6
Morgan CUGERONE <morgaan@gmail.com>
2023-11-20 13:44:57 +0100

    Add some more abbreviations for git issue number + Co-authored-by... Chrissa

--- .config/nvim/lua/morgan/abbreviations.lua
+++ .config/nvim/lua/morgan/abbreviations.lua
@@ -1,3 +1,5 @@
 local cmd = vim.api.nvim_command
 
+cmd('iabbr giti Issue: springernature/pegasus#')
 cmd('iabbr coauth Co-authored-by: Daniel Clubb <daniel.clubb@springernature.com><CR>Co-authored-by: Morgan Cugerone <morgan.cugerone@springernature.com><CR>Co-authored-by: Chrissa Kaza <chrysoula.kaza@springernature.com>')
+cmd('iabbr cochrissa Co-authored-by: Chrissa Kaza <chrysoula.kaza@springernature.com>')
