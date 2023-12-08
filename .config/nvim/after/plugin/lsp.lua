ec6ffbbd64e59f71c3e3a93d62c2f82b5a5b14aa
Morgan CUGERONE <morgaan@gmail.com>
2023-12-08 15:06:09 +0100

    Fix LSP issues

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -10,7 +10,7 @@ require('mason').setup({
 })
 
 mason_lspconfig.setup({
-	ensure_installed = { 'lua_ls' }
+	ensure_installed = { 'lua_ls', 'tsserver', 'eslint', 'kotlin_language_server' }
 })
 
 local on_attach = function(_, bufnr)
--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
 	-- A list of parser names, or 'all'
-	ensure_installed = { 'help', 'javascript', 'css', 'scss', 'html', 'markdown', 'json', 'vim', 'yaml', 'jsdoc', 'graphql', 'gitignore', 'gitcommit', 'git_rebase', 'dockerfile', 'diff', 'comment', 'bash', 'lua'},
+	ensure_installed = { 'javascript', 'css', 'scss', 'html', 'markdown', 'json', 'vim', 'yaml', 'jsdoc', 'graphql', 'gitignore', 'gitcommit', 'git_rebase', 'dockerfile', 'diff', 'comment', 'bash', 'lua'},
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
