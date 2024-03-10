8b43589b380a647c7b97d84fdf2d969030dc6bc8
Morgan CUGERONE <morgaan@gmail.com>
2024-03-10 23:21:07 +0100

    Make markdown-preview config universal

--- .config/nvim/lua/morgan/plugins/markdown-preview.lua
+++ .config/nvim/lua/morgan/plugins/markdown-preview.lua
@@ -7,8 +7,12 @@ return {
 
 			global.mkdp_auto_start = 0
 			global.mkdp_auto_close = 1
-			-- global.mkdp_browser = 'Firefox Developer Edition'
-			global.mkdp_browser = '/opt/firefox-developer-edition/firefox'
+
+			if (vim.loop.os_uname().sysname == "Linux") then
+				global.mkdp_browser = '/opt/firefox-developer-edition/firefox'
+			else
+				global.mkdp_browser = 'Firefox Developer Edition'
+			end
 		end,
 		build = function()
 			vim.fn['mkdp#util#install']()
