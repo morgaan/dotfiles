a86ce9556ad4ef29b500235cbd280bab7e7cd53e
Morgan CUGERONE <morgaan@gmail.com>
2024-10-09 23:58:55 +0200

    Update vim abbreviations

--- .config/nvim/lua/morgan/config/abbreviations.lua
+++ .config/nvim/lua/morgan/config/abbreviations.lua
@@ -1,10 +1,17 @@
 local cmd = vim.api.nvim_command
 
 cmd('iabbr giti Issue: springernature/pegasus#')
-cmd('iabbr coauth Co-authored-by: Daniel Clubb <daniel.clubb@springernature.com><CR>Co-authored-by: Morgan Cugerone <morgan.cugerone@springernature.com><CR>Co-authored-by: Chrissa Kaza <chrysoula.kaza@springernature.com>')
+
+cmd('iabbr cofeds Co-authored-by: Daniel Clubb <daniel.clubb@springernature.com><CR>Co-authored-by: Morgan Cugerone <morgan.cugerone@springernature.com><CR>Co-authored-by: Chrissa Kaza <chrysoula.kaza@springernature.com>')
 cmd('iabbr cochris Co-authored-by: Chrissa Kaza <chrysoula.kaza@springernature.com>')
 cmd('iabbr codan Co-authored-by: Daniel Clubb <daniel.clubb@springernature.com>')
-cmd('iabbr coarun Co-authored-by: Arun Prasath <arunprasath.tamilselvan@springernature.com>')
-cmd('iabbr comarkus Co-authored-by: Markus Hampel <markus.hampel@springer.com>')
 cmd('iabbr coben Co-authored-by: Ben Clark <hello@clarkben.com>')
-cmd('iabbr codeepali Co-authored-by: Deepali Dubey <deepali.dubey@springer.com>')
+cmd('iabbr codeepali Co-authored-by: Deepali Dubey <deepali.dubey@springernature.com>')
+
+cmd('iabbr coarun Co-authored-by: Arun Prasath <arunprasath.tamilselvan@springernature.com>')
+
+cmd('iabbr cobruno Co-authored-by: Bruno Amorim <bruno.amorim@springernature.com>')
+cmd('iabbr coluis Co-authored-by: Luis Silva <luis.silva@springernature.com>')
+cmd('iabbr codiogo Co-authored-by: diogo.matos@springernature.com')
+cmd('iabbr cojoerg Co-authored-by: joerg.liedl@springernature.com')
+cmd('iabbr comarkus Co-authored-by: Markus Hampel <markus.hampel@springer.com>')
