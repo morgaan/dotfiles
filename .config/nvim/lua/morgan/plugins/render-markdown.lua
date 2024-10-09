6a04b96805380c9285fdd017ffb2aeafbf9f8165
Morgan CUGERONE <morgaan@gmail.com>
2024-10-09 23:59:36 +0200

    Comment out render-markdown for now

--- .config/nvim/lua/morgan/plugins/render-markdown.lua
+++ .config/nvim/lua/morgan/plugins/render-markdown.lua
@@ -1,35 +1,36 @@
 -- https://github.com/MeanderingProgrammer/render-markdown.nvim
-return {
-	'MeanderingProgrammer/render-markdown.nvim',
-	event = {'BufReadPre', 'BufNewFile'},
-	dependencies = {'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons'},
-	opts = {
-		anti_conceal = {
-			enabled = true
-		},
-		heading = {
-			enabled = true,
-			width = 'block',
-			-- Could not find where these are coming from, not from
-			-- `nvim-web-devicons` as it seems not to be listed in
-			-- `:NvimWebDeviconsHiTest`
-			-- icons = { '󰲡 ', '󰲣 ', '󰲥 ', '󰲧 ', '󰲩 ', '󰲫 ' },
-			icons = {},
-		},
-		link = {
-			enabled = true,
-			image = '󰥶 ',
-			email = '󰀓 ',
-			hyperlink = '󰌹 ',
-			highlight = 'RenderMarkdownLink',
-			custom = {
-				web = { pattern = '^http[s]?://', icon = '󰖟 ', highlight = 'RenderMarkdownLink' },
-			},
-		},
-		code = {
-			enabled = true,
-			position = 'left',
-			width = 'block'
-		}
-	}
-}
+-- return {
+-- 	'MeanderingProgrammer/render-markdown.nvim',
+-- 	event = {'BufReadPre', 'BufNewFile'},
+-- 	dependencies = {'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons'},
+-- 	opts = {
+-- 		render_modes = { 'n', 'c' },
+-- 		anti_conceal = {
+-- 			enabled = true
+-- 		},
+-- 		heading = {
+-- 			enabled = true,
+-- 			width = 'block',
+-- 			-- Could not find where these are coming from, not from
+-- 			-- `nvim-web-devicons` as it seems not to be listed in
+-- 			-- `:NvimWebDeviconsHiTest`
+-- 			-- icons = { '󰲡 ', '󰲣 ', '󰲥 ', '󰲧 ', '󰲩 ', '󰲫 ' },
+-- 			icons = {},
+-- 		},
+-- 		link = {
+-- 			enabled = true,
+-- 			image = '󰥶 ',
+-- 			email = '󰀓 ',
+-- 			hyperlink = '󰌹 ',
+-- 			highlight = 'RenderMarkdownLink',
+-- 			custom = {
+-- 				web = { pattern = '^http[s]?://', icon = '󰖟 ', highlight = 'RenderMarkdownLink' },
+-- 			},
+-- 		},
+-- 		code = {
+-- 			enabled = true,
+-- 			position = 'left',
+-- 			width = 'block'
+-- 		}
+-- 	}
+-- }
