61686d0959aee2dcd694f30ae5224b681759aca7
Morgan CUGERONE <morgaan@gmail.com>
2024-10-28 20:23:33 +0100

    Override colorscheme colours for better markdown rendering

--- .config/nvim/lua/morgan/plugins/colorscheme.lua
+++ .config/nvim/lua/morgan/plugins/colorscheme.lua
@@ -6,5 +6,10 @@ return {
 	config = function()
 		-- colorscheme catppuccin " catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
 		vim.cmd([[colorscheme catppuccin-frappe]])
+
+		-- Overrides and add-ons
+		vim.cmd([[highlight @markup.quote cterm=NONE gui=NONE guifg=#ea999c]])
+		vim.cmd([[highlight @text.emphasis cterm=italic gui=italic guifg=#a6d189]])
+		vim.cmd([[highlight RenderMarkdownHighlight guifg=#303446 guibg=#f2d5cf]])
 	end
 }
