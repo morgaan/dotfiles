744af5cbc6e103f1da1896d5ae37ce0bececaddf
Morgan CUGERONE <morgaan@gmail.com>
2025-01-02 17:42:55 +0100

    Mimics Obsidian inline highlights when content is surrounded by double equals

--- .config/nvim/lua/morgan/plugins/render-markdown.lua
+++ .config/nvim/lua/morgan/plugins/render-markdown.lua
@@ -1,6 +1,7 @@
 -- https://github.com/MeanderingProgrammer/render-markdown.nvim
 return {
 	'MeanderingProgrammer/render-markdown.nvim',
+	commit = '3a319cd',
 	event = {'BufReadPre', 'BufNewFile'},
 	dependencies = {'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons'},
 	opts = {
@@ -80,57 +81,65 @@ return {
 				delegated = { raw = '[d]', rendered = '  ', highlight = 'RenderMarkdownChecked', scope_highlight = nil }
 			}
 		},
-		custom_handlers = {
-			markdown = {
-			extends = true,
-			parse = function(root, buf)
-				local marks = {}
+        -- Mimics Obsidian inline highlights when content is surrounded by double equals
+        -- The equals on both ends are concealed and the inner content is highlighted
+        inline_highlight = {
+            -- Turn on / off inline highlight rendering
+            enabled = true,
+            -- Applies to background of surrounded text
+            highlight = 'RenderMarkdownCustomHighlight',
+        },
+		--custom_handlers = {
+		--	markdown = {
+		--	extends = true,
+		--	parse = function(root, buf)
+		--		local marks = {}
 
-				---@param row { [1]: integer, [2]: integer }
-				---@param col { [1]: integer, [2]: integer }
-				---@param conceal? string
-				---@param hl_group? string
-				local function append(row, col, conceal, hl_group)
-				table.insert(marks, {
-					conceal = row[1] == row[2],
-					start_row = row[1],
-					start_col = col[1],
-					opts = { end_row = row[2], end_col = col[2], conceal = conceal, hl_group = hl_group },
-				})
-				end
+		--		---@param row { [1]: integer, [2]: integer }
+		--		---@param col { [1]: integer, [2]: integer }
+		--		---@param conceal? string
+		--		---@param hl_group? string
+		--		local function append(row, col, conceal, hl_group)
+		--		table.insert(marks, {
+		--			conceal = row[1] == row[2],
+		--			start_row = row[1],
+		--			start_col = col[1],
+		--			opts = { end_row = row[2], end_col = col[2], conceal = conceal, hl_group = hl_group },
+		--		})
+		--		end
 
-				local text = vim.treesitter.get_node_text(root, buf)
-				local top_row = root:range()
+		--		local text = vim.treesitter.get_node_text(root, buf)
+		--		local top_row = root:range()
 
-				---@param index integer
-				---@return integer, integer
-				local function row_col(index)
-				local lines = vim.split(text:sub(1, index), '\n', { plain = true })
-				return top_row + #lines - 1, #lines[#lines]
-				end
+		--		---@param index integer
+		--		---@return integer, integer
+		--		local function row_col(index)
+		--		local lines = vim.split(text:sub(1, index), '\n', { plain = true })
+		--		return top_row + #lines - 1, #lines[#lines]
+		--		end
 
-				---@type integer|nil
-				local index = 1
-				while index ~= nil do
-				local start_index, end_index = text:find('(=)=[^=]+=(=)', index)
-				if start_index ~= nil and end_index ~= nil then
-					local start_row, start_col = row_col(start_index - 1)
-					local end_row, end_col = row_col(end_index)
-					-- Hide first 2 equal signs
-					append({ start_row, start_row }, { start_col, start_col + 2 }, '', nil)
-					-- Highlight contents
-					append({ start_row, end_row }, { start_col, end_col }, nil, 'RenderMarkdownCustomHighlight')
-					-- Hide last 2 equal signs
-					append({ end_row, end_row }, { end_col - 2, end_col }, '', nil)
-					index = end_index + 1
-				else
-					index = nil
-				end
-				end
+		--		---@type integer|nil
+		--		local index = 1
+		--		while index ~= nil do
+		--		local start_index, end_index = text:find('(=)=[^=]+=(=)', index)
+		--		if start_index ~= nil and end_index ~= nil then
+		--			local start_row, start_col = row_col(start_index - 1)
+		--			local end_row, end_col = row_col(end_index)
+		--			-- Hide first 2 equal signs
+		--			append({ start_row, start_row }, { start_col, start_col + 2 }, '', nil)
+		--			-- Highlight contents
+		--			append({ start_row, end_row }, { start_col, end_col }, nil, 'RenderMarkdownCustomHighlight')
+		--			-- Hide last 2 equal signs
+		--			append({ end_row, end_row }, { end_col - 2, end_col }, '', nil)
+		--			index = end_index + 1
+		--		else
+		--			index = nil
+		--		end
+		--		end
 
-				return marks
-			end,
-			}
-		}
+		--		return marks
+		--	end,
+		--	}
+		--}
 	}
 }
