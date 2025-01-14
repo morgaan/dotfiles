6bbd635d1eb2ea93d492109d2f6b85d0f6b6f598
Morgan CUGERONE <morgaan@gmail.com>
2025-01-15 00:15:06 +0100

    Fix long lasting Obsidian bug where `[..` autocompletion would show so error notification

--- .config/nvim/lua/morgan/plugins/obsidian.lua
+++ .config/nvim/lua/morgan/plugins/obsidian.lua
@@ -33,8 +33,8 @@ return {
 	completion = {
 		-- Set to false to disable completion.
 		nvim_cmp = true,
-		-- Trigger completion at 2 chars.
-		min_chars = 2,
+		-- Trigger completion at 3 chars.
+		min_chars = 3,
 	},
 
 	preferred_link_style = "markdown",
