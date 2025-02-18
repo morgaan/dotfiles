2fe7cfa3c5b311a1e707b94094e041e1736c44bf
Morgan CUGERONE <morgaan@gmail.com>
2025-02-19 00:18:08 +0100

    Simplify harpoon mappings

--- .config/nvim/lua/morgan/plugins/harpoon.lua
+++ .config/nvim/lua/morgan/plugins/harpoon.lua
@@ -8,15 +8,15 @@ return {
 		local mark = require('harpoon.mark')
 		local ui = require('harpoon.ui')
 
-		keymap('n', '<leader>ha', mark.add_file, { desc = '[H]arpoon: [A]dd file' })
-		keymap('n', '<leader>hq', ui.toggle_quick_menu, { desc = '[H]arpoon: Toggle quick menu to [O]rganise' })
+		keymap('n', 'ha', mark.add_file, { desc = '[H]arpoon: [A]dd file' })
+		keymap('n', 'hm', ui.toggle_quick_menu, { desc = '[H]arpoon [M]enu' })
 		keymap('n', '[h', ui.nav_prev, { desc = '[H]arpoon: Nav previous' })
 		keymap('n', ']h', ui.nav_next, { desc = '[H]arpoon: Nav next' })
-		-- Below should read `number` + `leader` + `h` (e.g. 2<leader>h will
+		-- Below should read `number` + `h` + `n` (e.g. 2hn will
 		-- switch to 2nd buffer in Harpoon marks
-		keymap('n', '<leader>h', function() ui.nav_file(vim.v.count1) end, { desc = 'Harpoon: Navigate to file i(where is the count preceding the key mapping' })
+		keymap('n', 'hh', function() ui.nav_file(vim.v.count1) end, { desc = 'Harpoon: Navigate to file i(where is the count preceding the key mapping' })
 
 		-- Telescope integration
-		keymap('n', '<leader>hm', require('telescope').extensions.harpoon.marks, { desc = 'Harpoon: [H]arpoon [M]arks' })
+		keymap('n', 'ht', require('telescope').extensions.harpoon.marks, { desc = 'Harpoon: [H]arpoon marks in [T]elescope' })
 	end
 }
