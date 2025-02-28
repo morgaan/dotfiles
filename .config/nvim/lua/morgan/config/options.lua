73ff273708f7ece0d9e66b51e43f8a694b52d254
Morgan CUGERONE <morgaan@gmail.com>
2025-02-28 18:13:48 +0100

    Sync unnammed and `+` register in Vim for better QoL

--- .config/nvim/lua/morgan/config/options.lua
+++ .config/nvim/lua/morgan/config/options.lua
@@ -7,6 +7,9 @@ local winopt = vim.wo
 opt.encoding = 'utf-8'
 opt.mouse = ''
 
+-- Improved clipboard management
+vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
+
 -- Spell check
 opt.spelllang = {'en', 'fr', 'de'}
 opt.spell = true
