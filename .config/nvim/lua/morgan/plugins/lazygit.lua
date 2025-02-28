f94e5dda0f513cea35921121eb52ab8a319ad308
Morgan CUGERONE <morgaan@gmail.com>
2025-02-28 18:17:49 +0100

    Add LazyGit plugin

---
+++ .config/nvim/lua/morgan/plugins/lazygit.lua
@@ -0,0 +1,27 @@
+-- https://github.com/kdheepak/lazygit.nvim
+-- Install it on you Linux Ubuntu machine with:
+--
+-- LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+-- curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+-- tar xf lazygit.tar.gz lazygit
+-- sudo install lazygit -D -t /usr/local/bin/
+return {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+}
