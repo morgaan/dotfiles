ab216b0d6f402beaa4396d65de20d29c9b3d39c9
Morgan CUGERONE <morgaan@gmail.com>
2025-09-26 11:08:28 +0200

    Update Dan's email

--- .config/nvim/lua/morgan/config/abbreviations.lua
+++ .config/nvim/lua/morgan/config/abbreviations.lua
@@ -2,9 +2,9 @@ local cmd = vim.api.nvim_command
 
 cmd('cabbrev Qa qa')
 
-cmd('iabbr cofeds Co-authored-by: Daniel Clubb <daniel.clubb@springernature.com><CR>Co-authored-by: Morgan Cugerone <morgan.cugerone@springernature.com><CR>Co-authored-by: Chrissa Kaza <chrysoula.kaza@springernature.com>')
+cmd('iabbr cofeds Co-authored-by: Daniel Clubb <daniel.clubb.consultant@springernature.com><CR>Co-authored-by: Morgan Cugerone <morgan.cugerone@springernature.com><CR>Co-authored-by: Chrissa Kaza <chrysoula.kaza@springernature.com>')
 cmd('iabbr cochris Co-authored-by: Chrissa Kaza <chrysoula.kaza@springernature.com>')
-cmd('iabbr codan Co-authored-by: Daniel Clubb <daniel.clubb@springernature.com>')
+cmd('iabbr codan Co-authored-by: Daniel Clubb <daniel.clubb.consultant@springernature.com>')
 cmd('iabbr coben Co-authored-by: Ben Clark <hello@clarkben.com>')
 cmd('iabbr codeepali Co-authored-by: Deepali Dubey <deepali.dubey@springernature.com>')
 
