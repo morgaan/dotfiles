09dbffd58e07ba9759c47716794696872d2139cf
Morgan CUGERONE <morgaan@gmail.com>
2025-12-05 09:14:55 +0100

    Increase formatters timeout

--- .config/nvim/lua/morgan/plugins/conform.lua
+++ .config/nvim/lua/morgan/plugins/conform.lua
@@ -29,7 +29,7 @@ return {
 			format_on_save = {
 				lsp_fallback = false,
 				async = false,
-				timeout_ms = 1000,
+				timeout_ms = 3000,
 			},
 		})
 
@@ -37,7 +37,7 @@ return {
 			conform.format({
 				lsp_fallback = true,
 				async = false,
-				timeout_ms = 500,
+				timeout_ms = 3000,
 			})
 		end, { desc = "Format file or range (in visual mode)" })
 	end,
