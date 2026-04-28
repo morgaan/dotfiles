1c3d42546331529568a1c1df143c42aada9ca1e9
Morgan CUGERONE <morgaan@gmail.com>
2026-04-28 14:45:35 +0200

    Enable toggle hidden on Obsidian notes assets folder

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -4,12 +4,24 @@ return {
 		delete_to_trash = true,
 		view_options = {
 			show_hidden = true,
-			natural_order = false
+			natural_order = false,
+			is_hidden_file = function(name, bufnr)
+				local is_dotfile = vim.startswith(name, ".") and name ~= ".."
+
+				-- `.assets` directories are where I store notes attachments in
+				-- my Obsidian Second Brain.
+				local is_assets_dir = vim.endswith(name, ".assets")
+				if is_assets_dir then
+					return true
+				end
+
+				return is_dotfile
+			end,
 		},
 		keymaps = {
 			['<C-p>'] = false, -- avoid override of Telescope search keymap.
 			['<leader>p'] = 'actions.preview' -- replace by leader p
-		}
+		},
 	},
 	-- Optional dependencies
 	dependencies = { "nvim-tree/nvim-web-devicons" },
