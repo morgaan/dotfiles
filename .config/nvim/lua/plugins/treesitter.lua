79fab9f7d2b9521c089c6b25f9b3012a5277a4a9
Morgan CUGERONE <morgaan@gmail.com>
2026-05-20 12:24:48 +0200

    Update treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,39 +1,11 @@
+-- https://github.com/nvim-treesitter/nvim-treesitter
+-- Requires tree-sitter CLI available at https://github.com/tree-sitter/tree-sitter/releases/latest
 -- Highlight, edit, and navigate code
 return {
-	{
-		'nvim-treesitter/nvim-treesitter',
-		event = {'BufReadPre', 'BufNewFile'},
-		config = function()
-			require('nvim-treesitter.configs').setup {
-				-- A list of parser names, or 'all'
-				ensure_installed = { 'javascript', 'css', 'scss', 'html', 'markdown', 'markdown_inline', 'json', 'vim', 'yaml', 'jsdoc', 'graphql', 'gitignore', 'gitcommit', 'git_rebase', 'dockerfile', 'diff', 'comment', 'bash', 'lua'},
-
-				-- Install parsers synchronously (only applied to `ensure_installed`)
-				sync_install = false,
-
-				-- Automatically install missing parsers when entering buffer
-				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-				auto_install = true,
-
-				highlight = {
-					-- `false` will disable the whole extension
-					enable = true,
-
-					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-					-- Using this option may slow down your editor, and you may see some duplicate highlights.
-					-- Instead of true it can also be a list of languages
-					additional_vim_regex_highlighting = false
-				},
-				indent = {
-					enable = true,
-					disable = { '' }
-				}
-			}
-
-		end,
-		build = function()
-			pcall(require('nvim-treesitter.install').update { with_sync = true })
-		end
-	}
+	'nvim-treesitter/nvim-treesitter',
+	lazy = false,
+	build = ':TSUpdate',
+	config = function()
+		require('nvim-treesitter').install { 'javascript', 'css', 'scss', 'html', 'markdown', 'markdown_inline', 'json', 'vim', 'yaml', 'jsdoc', 'graphql', 'gitignore', 'gitcommit', 'git_rebase', 'dockerfile', 'diff', 'comment', 'bash', 'lua' }
+	end
 }
