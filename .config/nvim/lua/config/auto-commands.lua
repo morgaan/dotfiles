9d95b50ab4528af20e9f8d89da88f6bac5debc32
Morgan CUGERONE <morgaan@gmail.com>
2026-07-09 17:11:24 +0200

    Add support for fancy quotes text objects

--- .config/nvim/lua/config/auto-commands.lua
+++ .config/nvim/lua/config/auto-commands.lua
@@ -2,50 +2,50 @@
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*'
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = '*'
 })
 
 -- Force shell interactive mode so that .zshrc commands are known in Vim.
 local vimenter_group = vim.api.nvim_create_augroup('vimenter_group', { clear = true })
-vim.api.nvim_create_autocmd({'VimEnter'}, {
-  command = "let &shell='/bin/zsh -i'",
-  group = vimenter_group,
-  pattern = '*'
+vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+	command = "let &shell='/bin/zsh -i'",
+	group = vimenter_group,
+	pattern = '*'
 })
 
 local netrw_mappings = vim.api.nvim_create_augroup('netrw_mappings', { clear = true })
-vim.api.nvim_create_autocmd({'Filetype'}, {
-  callback = function()
-	  vim.keymap.set('n', '<C-h>', '<cmd>wincmd h<cr>', { buffer=true })
-	  vim.keymap.set('n', '<C-j>', '<cmd>wincmd j<cr>', { buffer=true })
-	  vim.keymap.set('n', '<C-k>', '<cmd>wincmd k<cr>', { buffer=true })
-	  vim.keymap.set('n', '<C-l>', '<cmd>wincmd l<cr>', { buffer=true })
-  end,
-  group = netrw_mappings,
-  pattern = 'netrw'
+vim.api.nvim_create_autocmd({ 'Filetype' }, {
+	callback = function()
+		vim.keymap.set('n', '<C-h>', '<cmd>wincmd h<cr>', { buffer = true })
+		vim.keymap.set('n', '<C-j>', '<cmd>wincmd j<cr>', { buffer = true })
+		vim.keymap.set('n', '<C-k>', '<cmd>wincmd k<cr>', { buffer = true })
+		vim.keymap.set('n', '<C-l>', '<cmd>wincmd l<cr>', { buffer = true })
+	end,
+	group = netrw_mappings,
+	pattern = 'netrw'
 })
 
 local changes_tracking = vim.api.nvim_create_augroup('ChangesTracking', { clear = true })
-vim.api.nvim_create_autocmd({'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI'}, {
-  command = 'silent! checktime',
-  group = changes_tracking,
-  pattern = '*'
+vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
+	command = 'silent! checktime',
+	group = changes_tracking,
+	pattern = '*'
 })
-vim.api.nvim_create_autocmd({'FileChangedShellPost'}, {
-  command = 'echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None',
-  group = changes_tracking,
-  pattern = '*'
+vim.api.nvim_create_autocmd({ 'FileChangedShellPost' }, {
+	command = 'echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None',
+	group = changes_tracking,
+	pattern = '*'
 })
 
 local on_resize = vim.api.nvim_create_augroup('OnVimResize', { clear = true })
-vim.api.nvim_create_autocmd({'VimResized'}, {
-  command = 'wincmd =',
-  group = on_resize,
-  pattern = '*'
+vim.api.nvim_create_autocmd({ 'VimResized' }, {
+	command = 'wincmd =',
+	group = on_resize,
+	pattern = '*'
 })
 
 -- Source: Prevent Vim from breaking up links mid-tag in markdown
@@ -67,3 +67,11 @@ vim.api.nvim_exec([[
 		endif
 	endfunction
 ]], false)
+
+-- Enables `preservim/vim-textobj-quote` plugin in Markdown files
+vim.cmd([[
+	augroup textobj_quote
+		autocmd!
+		autocmd FileType markdown call textobj#quote#init()
+	augroup END
+]])
--- .config/nvim/lua/plugins/text-objects.lua
+++ .config/nvim/lua/plugins/text-objects.lua
@@ -1,30 +1,38 @@
 return {
 	{
 		'wellle/targets.vim',
-		event = {'BufReadPre', 'BufNewFile'}
+		event = { 'BufReadPre', 'BufNewFile' }
 	},
 	{
 		'michaeljsmith/vim-indent-object',
-		event = {'BufReadPre', 'BufNewFile'}
+		event = { 'BufReadPre', 'BufNewFile' }
 	},
 	{
 		'kana/vim-textobj-entire',
-		event = {'BufReadPre', 'BufNewFile'},
-		dependencies = {'kana/vim-textobj-user'}
+		event = { 'BufReadPre', 'BufNewFile' },
+		dependencies = { 'kana/vim-textobj-user' }
 	},
 	{
 		'jceb/vim-textobj-uri',
-		event = {'BufReadPre', 'BufNewFile'},
-		dependencies = {'kana/vim-textobj-user'}
+		event = { 'BufReadPre', 'BufNewFile' },
+		dependencies = { 'kana/vim-textobj-user' }
 	},
 	{
 		'kana/vim-textobj-line',
-		event = {'BufReadPre', 'BufNewFile'},
-		dependencies = {'kana/vim-textobj-user'}
+		event = { 'BufReadPre', 'BufNewFile' },
+		dependencies = { 'kana/vim-textobj-user' }
 	},
 	{
 		'whatyouhide/vim-textobj-xmlattr',
-		event = {'BufReadPre', 'BufNewFile'},
-		dependencies = {'kana/vim-textobj-user'}
+		event = { 'BufReadPre', 'BufNewFile' },
+		dependencies = { 'kana/vim-textobj-user' }
+	},
+	{
+		-- https://github.com/preservim/vim-textobj-quote
+		-- Beware: There is a command in my commands to enable this plugin on
+		-- Markdown files
+		'preservim/vim-textobj-quote',
+		event = { 'BufReadPre', 'BufNewFile' },
+		dependencies = { 'kana/vim-textobj-user' }
 	},
 }
